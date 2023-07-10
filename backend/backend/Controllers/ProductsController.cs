5417ae1e27e2241b471fa638eca3bf9b49543498
MonicApor <58206744+MonicApor@users.noreply.github.com>
2023-07-10 15:52:16 +0800

    First Full Stack Project

---
+++ backend/backend/Controllers/ProductsController.cs
@@ -0,0 +1,97 @@
+﻿using backend.Context;
+using backend.DTOs;
+using backend.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //add constructor 
+        private readonly ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //Now we have access to the DB, Create the CRUD methods
+
+        //Create (IActionResult will not return anything)
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct([FromBody] CreateUpdateProductDto dto)
+        {
+            var newProduct = new ProductEntity()
+            {
+                Brand = dto.Brand,
+                Title = dto.Title,
+            };
+
+            await _context.Products.AddAsync(newProduct);
+            await _context.SaveChangesAsync();
+
+            return Ok("Product has been added successfully.");
+        }
+
+        //Read
+        [HttpGet]
+        public async Task<ActionResult<List<ProductEntity>>> GetAllProducts()
+        {
+            var products = await _context.Products.OrderByDescending(x => x.UpdatedAt).ToListAsync();
+
+            return Ok(products);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<ProductEntity>> GetProductByID([FromRoute] long id)
+        {
+            var product = await _context.Products.FirstOrDefaultAsync(x => x.Id == id);
+            if (product == null)
+            {
+                return NotFound("Product Not Found.");
+            }
+
+            return Ok(product);
+        }
+
+        //Update
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> UpdateProduct([FromRoute] long id, [FromBody] CreateUpdateProductDto dto)
+        {
+            var product = await _context.Products.FirstOrDefaultAsync(x => x.Id == id);
+
+            if(product == null)
+            {
+                return NotFound("Product Not Found");
+            }
+
+            product.Title = dto.Title;
+            product.Brand = dto.Brand;
+            product.UpdatedAt = DateTime.Now;
+
+            await _context.SaveChangesAsync();
+
+            return Ok("Product Updated Successfully.");
+        }
+
+        //Delete
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> DeleteProduct([FromRoute] long id)
+        {
+            var product = await _context.Products.FirstOrDefaultAsync(x => x.Id == id);
+            if( product == null)
+            {
+                return NotFound("Product Not Found");    
+            }
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+            return Ok("Deleted Successfully");
+        }
+    }
+}
---
+++ backend/backend/DTOs/CreateUpdateProductDto.cs
@@ -0,0 +1,9 @@
+﻿namespace backend.DTOs
+{
+    public class CreateUpdateProductDto
+    {
+        public string Brand { get; set; }
+
+        public string Title { get; set; }
+    }
+}
